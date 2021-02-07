4e1b31c63d0e15ed8db0f55f10dddcf6fe520320
Megan Kuo <megan.kuo@live.com>
2021-02-06 18:15:46 -0800

    Added js for covid banner and modal

--- js/scripts.js
+++ js/scripts.js
@@ -45,3 +45,35 @@ const countryCode = document.getElementById('country');
 const postalCode = document.getElementById('postal-code');
 
 
+/**
+ * Show and hide COVID-19 precautions
+ */
+// Get the modal and banner
+const modal = document.getElementById("covid-modal");
+const banner = document.getElementById("covid-19-banner");
+
+const readMore = document.getElementById("btn-read-more");
+
+const closeBanner = document.getElementsByClassName("close-banner")[0];
+const exitModal = document.getElementsByClassName("close-modal")[0];
+
+// show modal
+readMore.onclick = function() {
+  modal.style.display = "block";
+}
+
+// close the modal
+exitModal.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+// hide COVID banner
+closeBanner.onclick = function() {
+  banner.style.display = "none";
+}
