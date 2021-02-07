6cce55daffaa22d5c52eacbe7b299512a8f4ddad
jfoxx77 <jsam07@hotmail.com>
2021-02-07 09:34:43 -0800

    Refactoring js/scripts.js for redability

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,25 @@
-var elem = document.querySelector('.tabs');
-var options = {};
-var instance = M.Tabs.init(elem, options);
-
-// For Form selects .....
-document.addEventListener('DOMContentLoaded', function () {
-	var elems = document.querySelectorAll('select');
-	M.FormSelect.init(elems, options);
-});
-
+/* Initialize parameters */
+let options = {};
+let ticketCount = 2;
+const elem = document.querySelector('.tabs');
+const instance = M.Tabs.init(elem, options);
 const form = document.getElementById('payment-form');
+const cancelBtn = document.getElementById('cancel');
 
+/* Event Listeners */
 form.addEventListener('submit', submit);
-document.getElementById('cancel').addEventListener('click', returnHome);
+cancelBtn.addEventListener('click', returnHome);
+document.addEventListener('DOMContentLoaded', initialize);
+
+
+/**
+ * Initializes bookings page to use materialize
+ */
+function initialize () {
+	const elems = document.querySelectorAll('select');
+	M.FormSelect.init(elems, options);
+}
+
 
 /**
  * Resets the payment form on submission and redirects user to main page
@@ -19,7 +27,7 @@ document.getElementById('cancel').addEventListener('click', returnHome);
  */
 function submit(e) {
 	e.preventDefault();
-	var toastHTML = '<span>Ticket Booked! Please check your email for ticket confirmation.</span>';
+	let toastHTML = '<span>Ticket Booked! Please check your email for ticket confirmation.</span>';
 	form.reset();
 	M.updateTextFields();
 	M.toast({ html: toastHTML, displayLength: 2000, completeCallback: returnHome });
@@ -32,13 +40,30 @@ function returnHome() {
 	window.open('../index.html', '_self');
 }
 
+function verifyTicketCount(){
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /**
  * @Megan - You can do form validation here
  */
-const email = document.getElementById('email');
-const cardNumber = document.getElementById('card-number');
-const expiry = document.getElementById('expiry');
-const CVC = document.getElementById('cvc');
-const countryCode = document.getElementById('country');
-const postalCode = document.getElementById('postal-code');
+// const email = document.getElementById('email');
+// const cardNumber = document.getElementById('card-number');
+// const expiry = document.getElementById('expiry');
+// const CVC = document.getElementById('cvc');
+// const countryCode = document.getElementById('country');
+// const postalCode = document.getElementById('postal-code');
 
